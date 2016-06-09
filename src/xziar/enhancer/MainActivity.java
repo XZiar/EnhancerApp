b2b38866159a39f5442872c142d0996190995a6d
Chen Zack <czktc2007@gmail.com>
2016-06-09 14:59:43 +0800

    basic implement of bottombar

--- src/xziar/enhancer/MainActivity.java
+++ src/xziar/enhancer/MainActivity.java
@@ -1,17 +1,63 @@
 package xziar.enhancer;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
-public class MainActivity extends Activity
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+		implements OnMenuTabClickListener
 {
+	private BottomBar bottombar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		bottombar = BottomBar.attach(this, savedInstanceState);
+		bottombar.setMaxFixedTabs(2);
+		bottombar.setItems(R.menu.bottombar);
+		bottombar.setOnMenuTabClickListener(this);
+		bottombar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorAccent));
+		bottombar.mapColorForTab(1, 0xFF5D4037);
+		bottombar.mapColorForTab(2, "#7B1FA2");
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState)
+	{
+		super.onSaveInstanceState(outState);
+
+		bottombar.onSaveInstanceState(outState);
+	}
+
+	@Override
+	public void onMenuTabSelected(int menuItemId)
+	{
+		switch (menuItemId)
+		{
+		case R.id.tasklist:
+			Toast.makeText(this, "tasklist", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.forum:
+			Toast.makeText(this, "forum", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.user:
+			Toast.makeText(this, "user", Toast.LENGTH_SHORT).show();
+			break;
+		}
+
+	}
+
+	@Override
+	public void onMenuTabReSelected(int menuItemId)
+	{
+		// TODO Auto-generated method stub
+
 	}
 }
