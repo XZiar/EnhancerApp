799dd3b43e94892dab75c581d6a680ad1bc98874
Chen Zack <czktc2007@gmail.com>
2016-06-11 04:21:29 +0800

    simple add page for company register

--- src/xziar/enhancer/activity/RegisterActivity.java
+++ src/xziar/enhancer/activity/RegisterActivity.java
@@ -3,35 +3,46 @@ package xziar.enhancer.activity;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.ProgressDialog;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.NumberPicker;
-import android.widget.TextView;
 import android.widget.Toast;
 import xziar.enhancer.R;
+import xziar.enhancer.fragment.CpnRegFragment;
 import xziar.enhancer.fragment.StuRegFragment;
 import xziar.enhancer.widget.WaitDialog;
 
 public class RegisterActivity extends Activity implements OnClickListener
 {
 	private EditText un, pwd;
-	private Button btn_register;
-	private TextView txt_reg;
+	private Button btn_register, btn_stu, btn_cpn;
+	private FragmentManager fragMan;
+	private Fragment currentFrag;
+	private StuRegFragment stuFrag;
+	private CpnRegFragment cpnFrag;
 
 	private void initWidget()
 	{
 		un = (EditText) findViewById(R.id.username);
 		pwd = (EditText) findViewById(R.id.password);
+		btn_stu = (Button) findViewById(R.id.btn_stu);
+		btn_stu.setOnClickListener(this);
+		btn_cpn = (Button) findViewById(R.id.btn_cpn);
+		btn_cpn.setOnClickListener(this);
 		btn_register = (Button) findViewById(R.id.btn_register);
 		btn_register.setOnClickListener(this);
 
-		FragmentManager fragMan = getFragmentManager();
-		Fragment frag = new StuRegFragment(this);
-		fragMan.beginTransaction().add(R.id.regcontent, frag).show(frag).commit();
+		fragMan = getFragmentManager();
+		stuFrag = new StuRegFragment(this);
+		cpnFrag = new CpnRegFragment(this);
+		fragMan.beginTransaction().add(R.id.regcontent, currentFrag = stuFrag)
+				.add(R.id.regcontent, cpnFrag).hide(cpnFrag).commit();
+		btn_stu.setBackgroundResource(R.color.colorAccent);
+		btn_cpn.setBackgroundResource(R.color.iron);
 	}
 
 	@Override
@@ -51,7 +62,7 @@ public class RegisterActivity extends Activity implements OnClickListener
 					un.getText().toString() + " & " + pwd.getText().toString()
 							+ " «Î…‘∫Û... ...");
 			waitDialog.show();
-			new android.os.Handler().postDelayed(new Runnable()
+			new Handler().postDelayed(new Runnable()
 			{
 				public void run()
 				{
@@ -59,9 +70,29 @@ public class RegisterActivity extends Activity implements OnClickListener
 				}
 			}, 3000);
 		}
-
+		else if(v == btn_stu)
+		{
+			btn_stu.setBackgroundResource(R.color.colorAccent);
+			btn_cpn.setBackgroundResource(R.color.iron);
+			changeFrag(stuFrag);
+		}
+		else if(v == btn_cpn)
+		{
+			btn_cpn.setBackgroundResource(R.color.colorAccent);
+			btn_stu.setBackgroundResource(R.color.iron);
+			changeFrag(cpnFrag);
+		}
 		else
 			Toast.makeText(this, v.getClass().getName(), Toast.LENGTH_SHORT)
 					.show();
+	}
+
+	private void changeFrag(Fragment frag)
+	{
+		if (currentFrag == frag)
+			return;
+		FragmentTransaction fragTrans = fragMan.beginTransaction()
+				.hide(currentFrag);
+		fragTrans.show(currentFrag = frag).commit();
 	}
 }
